1e433d018e0fe3f3ff6c5537745f21e4c078091c
jmanndev <jmanndev@gmail.com>
2016-11-25 01:18:12 +1100

    Finished challenge

---
+++ A Game of Threes/A Game of Threes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace A_Game_of_Threes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = 321231241;
+
+            int[] moveOptions = { 0, -1, 1 };
+
+            while (value != 1)
+            {
+                Console.WriteLine(value.ToString() + " " + moveOptions[value % 3].ToString());
+                value = (value + moveOptions[value % 3]) / 3;
+            }
+            Console.WriteLine("1");
+            Console.ReadKey();
+        }
+    }
+}
